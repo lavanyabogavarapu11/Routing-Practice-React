366cf7f7954e5d64f7fdbafa0a4fea669b723d30
lavanya <lavanyabogavarapu22@gmail.com>
2023-09-15 07:43:24 +0000

    Routing Practice App React First Commit

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+// Write your JS code here
+import './index.css'
+
+const About = () => (
+  <div className="about-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/about-blog-img.png"
+      alt="about"
+      className="about-image"
+    />
+    <h1 className="about">About</h1>
+    <p className="about-description">
+      I love to create! i am fronted developer
+    </p>
+  </div>
+)
+
+export default About
---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav className="nav-header">
+    <div className="logo-section">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/wave-logo-img.png"
+        className="wave-image"
+        alt="wave"
+      />
+      <h1 className="logo-heading"> Wave</h1>
+    </div>
+    <ul className="nav-menu">
+      <li>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/about">
+          About
+        </Link>
+      </li>
+      <li>
+        <Link className="nav-link" to="/contact">
+          Contact
+        </Link>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Header
