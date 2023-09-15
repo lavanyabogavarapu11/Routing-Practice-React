1d383c497ae34c697a778df353461c3ef564bc0a
lavanya <lavanyabogavarapu22@gmail.com>
2023-09-15 09:38:39 +0000

    Routing Second Commit

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -5,10 +5,10 @@ const NotFound = () => (
   <div className="not-found-container">
     <img
       src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
-      alt="not-found"
+      alt="not found"
       className="not-found-img"
     />
-    <h1 className="not-found"> Not Found</h1>
+    <h1 className="not-found-heading">Not Found</h1>
   </div>
 )
 
